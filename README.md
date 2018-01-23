c831dd38f8471ee4ab1f17cfa0aa8b16c2de3328
Tomos Ewe <tomosewe@gmail.com>
2018-01-23 22:59:38 +1300

    Add design template

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 Splashpage for tomosewe.com
 
-Using create-react-app
-and this tutorial: https://medium.freecodecamp.org/surge-vs-github-pages-deploying-a-create-react-app-project-c0ecbf317089
+Created with create-react-app with template from https://html5up.net/
+
+Resources for hosting on github pages: https://medium.freecodecamp.org/surge-vs-github-pages-deploying-a-create-react-app-project-c0ecbf317089
+
+Help with DNS and github pages: https://blog.mikevillis.com/simple-react-site-with-create-react-app-github-pages-travis-ci-and-cloudflare-8a859daa881
--- src/App.js
+++ src/App.js
@@ -1,18 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="wrapper">
+        <div id="bg" />
+        <div id="overlay" />
+        <div id="main">
+          <header id="header">
+            <h1>Tomos Ewe</h1>
+            <p>
+              Web Developer &nbsp;&bull;&nbsp; Auckland &nbsp;&bull;&nbsp; New Zealand
+            </p>
+            <nav>
+              <ul>
+                <li>
+                  <a href="https://tomosewe.com/blog" className="icon fa-rss">
+                    <span className="label">Blog</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.github.com/tomosewe"
+                    className="icon fa-github"
+                  >
+                    <span className="label">Github</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://twitter.com/tomosewe"
+                    className="icon fa-twitter"
+                  >
+                    <span className="label">Twitter</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/in/tomosewe/"
+                    className="icon fa-linkedin"
+                  >
+                    <span className="label">LinkedIn</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="mailto:tomosewe@gmail.com"
+                    className="icon fa-envelope-o"
+                  >
+                    <span className="label">Email</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <footer id="footer">
+            <span className="copyright">
+              &copy; Tomos Ewe &bull; Design template: {' '}
+              <a href="http://html5up.net">HTML5 UP</a>
+              .
+            </span>
+          </footer>
+
+        </div>
       </div>
     );
   }
